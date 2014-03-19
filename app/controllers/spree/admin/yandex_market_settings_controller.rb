900f8da4baf8058d29ff235e4fbbd534b281a11b
Damir Konkov <d4mk0f@gmail.com>
2014-03-19 15:35:29 +0400

    export files sorting fix

--- app/controllers/spree/admin/yandex_market_settings_controller.rb
+++ app/controllers/spree/admin/yandex_market_settings_controller.rb
@@ -20,15 +20,17 @@ class Spree::Admin::YandexMarketSettingsController < Spree::Admin::BaseControlle
   def export_files
     directory = File.join(Rails.root, 'public', 'yandex_market', '**', '*')
     # нельзя вызывать стат, не удостоверившись в наличии файла!!111
-    @export_files =  Dir[directory].map {|x| [File.basename(x), (File.file?(x) ? File.mtime(x) : 0)] }.
-      sort{|x,y| y.last <=> x.last }
+    @export_files =  Dir[directory].map {|x| [File.basename(x), (File.file?(x) ? File.mtime(x) : 0)] unless x =~ /.xml/ }
+    @export_files.delete(nil)
+    @export_files = @export_files.sort{|x,y| y.last <=> x.last }
     e = @export_files.find {|x| x.first == "yandex_market.xml" }
     @export_files.reject! {|x| x.first == "yandex_market.xml" }
     @export_files.unshift(e) unless e.blank?
     
     directory = File.join(Rails.root, 'public', 'wikimart', '**', '*')
-    @export_files_wikimart =  Dir[directory].map {|x| [File.basename(x), (File.file?(x) ? File.mtime(x) : 0)] }.
-      sort{|x,y| y.last <=> x.last }
+    @export_files_wikimart =  Dir[directory].map {|x| [File.basename(x), (File.file?(x) ? File.mtime(x) : 0)] unless x =~ /.xml/ }
+    @export_files_wikimart.delete(nil)
+    @export_files_wikimart = @export_files_wikimart.sort{|x,y| y.last <=> x.last }
     e = @export_files_wikimart.find {|x| x.first == "wikimart.xml" }
     @export_files_wikimart.reject! {|x| x.first == "wikimart.xml" }
     @export_files_wikimart.unshift(e) unless e.blank?
