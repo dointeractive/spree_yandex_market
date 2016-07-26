8b752349ab02da2b5421da9b77f0f9ee8d1ae900
Oleg Litvin <oleg.litvin@live.ru>
2016-07-26 18:57:35 +0300

    Get rid of Spree::Product#published_category?

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -54,11 +54,15 @@ module Export
             }
 
             xml.offers { # список товаров
-              products = Spree::Product.active.where(export_to_yandex_market: true).group_by_products_id
+              products = Spree::Product.active
+                .joins(:taxons).merge(Spree::Taxon.published)
+                .where(export_to_yandex_market: true)
+                .group_by_products_id
+
               products = products.on_hand if @config.preferred_wares == "on_hand"
               products = products.joins(:taxons).where(spree_taxons: { export_to_yandex_market: true })
               products.find_each do |product|
-                offer(xml, product, product.taxons.first) if !product.taxons.empty? and product.published_category?
+                offer(xml, product, product.taxons.first)
               end
             }
           }
