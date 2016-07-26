9e395972da659e4e1117941a865300d7afc89317
Oleg Litvin <oleg.litvin@live.ru>
2016-07-26 18:13:00 +0300

    Delete usage of Spree::Taxon#available_for_search?

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -6,21 +6,21 @@ module Export
   class YandexMarketExporter
     include Spree::Core::Engine.routes.url_helpers
     attr_accessor :host, :currencies
-    
+
     DEFAULT_OFFER = "simple"
 
     def helper
       @helper ||= ApplicationController.helpers
     end
-    
+
     def export
       @config = Spree::YandexMarket::Config.instance
       @host = @config.preferred_url.sub(%r[^http://],'').sub(%r[/$], '')
       ActionController::Base.asset_host = @config.preferred_url
-      
+
       @currencies = @config.preferred_currency.split(';').map{|x| x.split(':')}
       @currencies.first[1] = 1
-      
+
       # Nokogiri::XML::Builder.new({ :encoding =>"utf-8"}, SCHEME) do |xml|
       Nokogiri::XML::Builder.new(:encoding =>"utf-8") do |xml|
         xml.doc.create_internal_subset('yml_catalog',
@@ -29,32 +29,30 @@ module Export
         )
 
         xml.yml_catalog(:date => Time.now.to_s(:ym)) {
-          
+
           xml.shop { # описание магазина
             xml.name    @config.preferred_short_name
             xml.company @config.preferred_full_name
             xml.url     path_to_url('')
-            
+
             xml.currencies { # описание используемых валют в магазине
               @currencies && @currencies.each do |curr|
                 opt = {:id => curr.first, :rate => curr[1] }
                 opt.merge!({ :plus => curr[2]}) if curr[2] && ["CBRF","NBU","NBK","CB"].include?(curr[1])
                 xml.currency(opt)
               end
-            }        
-            
+            }
+
             xml.categories { # категории товара
               Spree::Taxonomy.all.each do |taxonomy|
                 taxonomy.root.self_and_descendants.published.where(export_to_yandex_market: true).find_each do |cat|
-                  if cat.available_for_search?
-                    @cat_opt = { :id => cat.id }
-                    @cat_opt.merge!({ :parentId => cat.parent_id}) unless cat.parent_id.blank?
-                    xml.category(@cat_opt){ xml  << cat.name }
-                  end
+                  @cat_opt = { :id => cat.id }
+                  @cat_opt.merge!({ :parentId => cat.parent_id}) unless cat.parent_id.blank?
+                  xml.category(@cat_opt){ xml  << cat.name }
                 end
               end
             }
-            
+
             xml.offers { # список товаров
               products = Spree::Product.active.where(export_to_yandex_market: true).group_by_products_id
               products = products.on_hand if @config.preferred_wares == "on_hand"
@@ -64,22 +62,22 @@ module Export
               end
             }
           }
-        } 
+        }
       end.to_xml
-      
+
     end
-    
-    
+
+
     private
-    
+
     def path_to_url(path)
       "http://#{@host.sub(%r[^http://],'')}/#{path.sub(%r[^/],'')}"
     end
-    
+
     def offer(xml,product, cat)
       offer_simple(xml, product, cat)
     end
-    
+
     def shared_xml(xml, product, cat)
       xml.url product_url(product, :host => @host)
       xml.price product.price
@@ -87,7 +85,7 @@ module Export
       xml.categoryId cat.id
       xml.picture path_to_url(CGI.escape(product.images.first.attachment.url(:product, false))) unless product.images.empty?
     end
-    
+
     def individual_xml(xml, product, cat, product_properties = {})
       xml.delivery            true
       xml.local_delivery_cost @config.preferred_local_delivery_cost unless @config.preferred_local_delivery_cost.blank?
@@ -107,6 +105,6 @@ module Export
         individual_xml(xml, product, cat, product_properties)
       }
     end
-    
+
   end
 end
