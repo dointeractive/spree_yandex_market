76068c45e1b8644b282967488a3ba35414dff258
Andrey Ignatov <a.ignatov@dointeractive.ru>
2016-10-21 16:46:53 +0300

    Fix yandex market export

--- lib/export/yandex_market_without_utm_exporter.rb
+++ lib/export/yandex_market_without_utm_exporter.rb
@@ -2,7 +2,7 @@ module Export
   class YandexMarketWithoutUtmExporter < YandexMarketExporter
     private
 
-    def prepare_product_url(product)
+    def get_product_url(product)
       product_url(product, host: @host)
     end
   end
